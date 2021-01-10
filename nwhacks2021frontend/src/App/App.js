d306a6fb68b106952d48483d67e09c1538925a25
mpchow <mattchow918@gmail.com>
2021-01-10 10:23:52 -0800

    fix routing

--- nwhacks2021frontend/src/App/App.js
+++ nwhacks2021frontend/src/App/App.js
@@ -2,11 +2,8 @@ import {useState, useEffect} from 'react';
 
 // Component Dependencies
 import SetPage from '../Components/SetPage/setPage';
-<<<<<<< HEAD
 import HomePage from '../Components/HomePage/HomePage';
-=======
 import LandingPage from '../Components/LandingPage/LandingPage'
->>>>>>> 3af63b03a9a0922b802cf6729c6689f62da6b250
 
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -36,19 +33,14 @@ let mockSet = {
 
 
 function App() {
-  const[userInfo, setUserInfo] = useState({});
-
-  const updateUserInfo = (data) => {
-    setUserInfo(data);
-  }
-
   return (
     <Router>
       <div>
-          <Route path="/set" render={(props) => <SetPage {...props} data={mockSet}  updateUser={updateUserInfo}/>}/>
-          <Route path="/home" render={(props) => <HomePage {...props}/>}/>
-          <Route path="/" render={(props) => <LandingPage {...props} data={mockSet}  updateUser={updateUserInfo}/>}/>
-
+        <Switch>
+          <Route path="/set" render={(props) => <SetPage {...props} data={mockSet} />}/>
+          <Route path="/home" render={(props) => <HomePage {...props} />}/>
+          <Route path="/" render={(props) => <LandingPage {...props} />}/>
+        </Switch>
       </div>
     </Router>
   );
